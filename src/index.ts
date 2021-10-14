a55333c2836457427bbad2dee1d2d9053a7f3865
alexdaube <alexdaube@gmail.com>
2021-10-14 07:19:16 -0400

    Simple implementation of postmessage api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,57 @@
-export type NumberParseable = (number | string | boolean) & {
-  readonly isNumberParseble: unique symbol;
-};
+enum ViewerAPIMessage {
+  START_AR = 'START_AR',
+  UPDATE_VARIANT = 'UPDATE_VARIANT',
+}
 
-export const isNumberParseable = (value: unknown): value is NumberParseable =>
-  !Number.isNaN(Number(value));
+interface UpdateVariantPayload {
+  modelVariant: string;
+}
+
+type Payload = UpdateVariantPayload;
+
+interface Message {
+  type: ViewerAPIMessage;
+  payload?: Payload;
+}
+
+export class Visao {
+  private readonly id: string;
+  private readonly viewerElement: HTMLIFrameElement | null;
+
+  constructor(id: string) {
+    this.id = id;
+    this.viewerElement = document.getElementById(id) as HTMLIFrameElement;
+    this.logInvalidViewerElement();
+  }
+
+  public showModelVariant(modelVariant: string): void {
+    console.log('CALLING show model variant!');
+    this.executeAction({
+      type: ViewerAPIMessage.UPDATE_VARIANT,
+      payload: {
+        modelVariant,
+      },
+    });
+  }
+
+  public startAR(): void {
+    console.log('CALLING start AR!');
+    this.executeAction({
+      type: ViewerAPIMessage.START_AR,
+    });
+  }
+
+  private executeAction(action: Message): void {
+    this.logInvalidViewerElement();
+
+    this.viewerElement?.contentWindow?.postMessage(JSON.stringify(action), '*');
+  }
+
+  private logInvalidViewerElement(): void {
+    if (!this.viewerElement) {
+      console.error(
+        `Visao: Viewer HTML element cannot be found in the DOM with the "id" ${this.id}`,
+      );
+    }
+  }
+}
